e537beb0676f5ec8557eb712585f69ce7846ac25
Christopher Lin <c.l@berkeley.edu>
2016-06-28 11:57:38 -0700

    Implemented and test ExprPredicate. At predicate is an ExprPredicate

--- src/core/util_classes/common_predicates.py
+++ src/core/util_classes/common_predicates.py
@@ -1,26 +1,120 @@
 from IPython import embed as shell
 from core.internal_repr.predicate import Predicate
+from core.util_classes.matrix import Vector2d
 from errors_exceptions import PredicateException
+from expr import AffExpr, EqExpr
 import numpy as np
+
 
 """
 This file implements the classes for commonly used predicates that are useful in a wide variety of
 typical domains.
 """
 
-class At(Predicate):
+DEFAULT_TOL=1e-8
+
+class ExprPredicate(Predicate):
+
+    """
+    Predicates which are defined by a target value for a set expression.
+    """
+
+    def __init__(self, name, expr, attr_inds, tol, params, expected_param_types):
+        """
+        attr2inds is a dictionary that maps each parameter name to a
+        list of (attr, active_inds) pairs. This defines the mapping
+        from the primitive predicates of the params to the inputs to
+        expr
+        """
+        super(ExprPredicate, self).__init__(name, params, expected_param_types)
+        self.expr = expr
+        self.attr_inds = attr_inds
+        self.tol = tol
+
+        self.x_dim = sum(len(active_inds)
+                         for p_attrs in attr_inds.values()
+                         for (_, active_inds) in p_attrs)
+        self.x = np.zeros(self.x_dim)
+
+    def get_param_vector(self, t):
+        i = 0
+        for p in self.params:
+            for attr, ind_arr in self.attr_inds[p.name]:
+                n_vals = len(ind_arr)
+                self.x[i:i+n_vals] = getattr(p, attr)[ind_arr, t]
+                i += n_vals
+        return self.x
+
     def test(self, time):
         if not self.is_concrete():
             return False
-        # verify time is valid
-        T = self.params[0].pose.shape[1]
-        if time < 0 or time > T - 1:
+        if time < 0:
             raise PredicateException("Out of range time for predicate '%s'."%self)
-        return np.array_equal(self.params[0].pose[:, time], self.params[1].pose[:, time])
+        try:
+            return self.expr.eval(self.get_param_vector(time), tol=self.tol)
+        except IndexError:
+            ## this happens with an invalid time
+            raise PredicateException("Out of range time for predicate '%s'."%self)
 
-class RobotAt(Predicate):
-    def test(self, time):
-        return True
+    def unpack(self, y):
+        """
+        Gradient returned in a similar form to attr_inds
+        {param_name: [(attr, (g1,...gi,...gn)]}
+        gi are in the same order as the attr_inds list
+        """
+        res = {}
+        i = 0
+        for p in self.params:
+            res[p.name] = []
+            for attr, ind_arr in self.attr_inds[p.name]:
+                n_vals = len(ind_arr)
+                res[p.name].append((attr, y[i:i+n_vals]))
+                i += n_vals
+        return res
+
+    def _grad(self, t):
+        return self.expr.grad(self.get_param_vector(t))
+
+
+class At(ExprPredicate):
+
+    def __init__(self, name, params, expected_param_types):
+        assert len(params) == 2
+        dims = -1
+        attr_inds = {}
+        for p in params:
+            p_inds = []
+            attr = None
+            if hasattr(p, "pose"):
+                attr = "pose"
+            elif hasattr(p, "value"):
+                attr = "value"
+            if attr is not None:
+                if p.is_defined():
+                    cur_dim = getattr(p, attr).shape[0]
+                else:
+                    if p.get_attr_type(attr) is Vector2d:
+                        cur_dim = 2
+                    else:
+                        raise PredicateException("attribute type not supported")
+                p_inds.append((attr, np.array(range(cur_dim))))
+
+            if dims == -1:
+                dims = cur_dim
+            else:
+                assert dims == cur_dim
+            attr_inds[p.name] = p_inds
+        A = np.c_[np.eye(dims), -np.eye(dims)]
+        b = np.zeros((dims, 1))
+        val = np.zeros((dims, 1))
+        aff_e = AffExpr(A, b)
+        e = EqExpr(aff_e, val)
+        tol=DEFAULT_TOL
+
+        super(At, self).__init__(name, e, attr_inds, tol, params, expected_param_types)
+
+class RobotAt(At):
+    pass
 
 class IsGP(Predicate):
     def test(self, time):
--- src/test/test_core/test_util_classes/test_common_predicates.py
+++ src/test/test_core/test_util_classes/test_common_predicates.py
@@ -1,37 +1,167 @@
 import unittest
 from core.internal_repr import parameter
+from core.util_classes.matrix import Vector2d
 from core.util_classes import common_predicates
 from errors_exceptions import PredicateException
+import expr
 import numpy as np
 
+## exprs for testing
+e1 = expr.Expr(lambda x: np.array([x]))
+e2 = expr.Expr(lambda x: np.power(x, 2))
+
 class TestCommonPredicates(unittest.TestCase):
-    def test_at(self):
+
+    def test_expr_pred(self):
+        ## test initialization: x_dim computed correctly
+        ## test
         attrs = {"name": ["can"], "pose": ["undefined"], "_type": ["Can"]}
         attr_types = {"name": str, "pose": int, "_type": str}
         p1 = parameter.Object(attrs, attr_types)
-        attrs = {"name": ["target"], "pose": ["undefined"], "_type": ["Target"]}
-        attr_types = {"name": str, "pose": int, "_type": str}
-        p2 = parameter.Object(attrs, attr_types)
+        p1.pose = np.array([[1, 2, 3], [2, 3, 1], [3, 1, 2]])
         attrs = {"name": ["sym"], "value": ["undefined"], "_type": ["Sym"]}
         attr_types = {"name": str, "value": int, "_type": str}
-        p3 = parameter.Symbol(attrs, attr_types)
+        p2 = parameter.Symbol(attrs, attr_types)
+        p2.value = np.array([2, 2, 2])
 
-        pred = common_predicates.At("testpred", [p1, p2, p3], ["Can", "Target", "Sym"])
+        ## ExprPred Construction
+        attr_inds = {"can": [("pose", np.array([0], dtype=np.int))], "sym": []}## sym not used
+        e = expr.EqExpr(e1, np.array([2]))
+        pred = common_predicates.ExprPredicate("expr_pred", e, attr_inds, 1e-6, [p1, p2], ["Can", "Sym"])
+
+        ## get_param_vector
+        self.assertEqual(pred.x_dim, 1)
+        self.assertTrue(np.allclose(pred.get_param_vector(0), [1]))
+        self.assertTrue(np.allclose(pred.get_param_vector(1), [2]))
+        self.assertTrue(np.allclose(pred.get_param_vector(2), [3]))
+
+        ## unpacking
+        unpacked = pred.unpack([10])
+        self.assertTrue("can" in unpacked)
+        self.assertTrue("sym" in unpacked)
+        self.assertEqual(len(unpacked["can"]), 1)
+        self.assertEqual(len(unpacked["sym"]), 0)
+        self.assertEqual(("pose", [10]), unpacked["can"][0])
+
+    def test_expr_pred_eq(self):
+        ## test (multiple tols)
+        ## grad
+        tols = [1e-8, 1e-4, 1e-2]
+
+        attrs = {"name": ["can"], "pose": ["undefined"], "_type": ["Can"]}
+        attr_types = {"name": str, "pose": float, "_type": str}
+        p1 = parameter.Object(attrs, attr_types)
+        p1.pose = np.array([[1, 2, 3], [1 + tols[0], 2 + tols[0], 3], [1 + tols[1], 2 + tols[1], 3]]).T
+        attrs = {"name": ["sym"], "value": ["undefined"], "_type": ["Sym"]}
+        attr_types = {"name": str, "value": float, "_type": str}
+        p2 = parameter.Symbol(attrs, attr_types)
+        p2.value = np.array([[1, 2], [1, 2], [1, 2]], dtype=np.float64).T
+
+
+        ## pred is p1.pose[:1] = p2.value
+        attr_inds = {"can": [("pose", np.array([0, 1], dtype=np.int))], "sym": [("value", np.array([0, 1], dtype=np.int))]}
+        A = np.array([[1, 1, -1, -1]])
+        b = np.array([0])
+
+        aff_e = expr.AffExpr(A, b)
+        e = expr.EqExpr(aff_e, np.array([[0.]]))
+        pred0 = common_predicates.ExprPredicate("eq_expr_pred", e, attr_inds, tols[0], [p1, p2], ["Can", "Sym"])
+        self.assertTrue(pred0.test(0))
+        self.assertFalse(pred0.test(1))
+        self.assertFalse(pred0.test(2))
+
+        pred1 = common_predicates.ExprPredicate("eq_expr_pred", e, attr_inds, tols[1], [p1, p2], ["Can", "Sym"])
+        self.assertTrue(pred1.test(0))
+        self.assertTrue(pred1.test(1))
+        self.assertFalse(pred1.test(2))
+
+        pred2 = common_predicates.ExprPredicate("eq_expr_pred", e, attr_inds, tols[2], [p1, p2], ["Can", "Sym"])
+        self.assertTrue(pred2.test(0))
+        self.assertTrue(pred2.test(1))
+        self.assertTrue(pred2.test(2))
+
+    def test_expr_pred_leq(self):
+        ## test (multiple tols)
+        ## grad
+        tols = [1e-8, 1e-4, 1e-2]
+
+        attrs = {"name": ["can"], "pose": ["undefined"], "_type": ["Can"]}
+        attr_types = {"name": str, "pose": float, "_type": str}
+        p1 = parameter.Object(attrs, attr_types)
+        p1.pose = np.array([[1, 2, 3], [1 + tols[0], 2 + tols[0], 3], [1 + tols[1], 2 + tols[1], 3]]).T
+        attrs = {"name": ["sym"], "value": ["undefined"], "_type": ["Sym"]}
+        attr_types = {"name": str, "value": float, "_type": str}
+        p2 = parameter.Symbol(attrs, attr_types)
+        p2.value = np.array([[1, 2], [1, 2], [1, 2]], dtype=np.float64).T
+
+
+        ## pred is p1.pose[:1] = p2.value
+        attr_inds = {"can": [("pose", np.array([0, 1], dtype=np.int))], "sym": [("value", np.array([0, 1], dtype=np.int))]}
+        A = np.array([[1, 1, -1, -1]])
+        b = np.array([0])
+
+        aff_e = expr.AffExpr(A, b)
+        e = expr.LEqExpr(aff_e, np.array([[0.]]))
+        pred0 = common_predicates.ExprPredicate("leq_pred", e, attr_inds, tols[0], [p1, p2], ["Can", "Sym"])
+        self.assertTrue(pred0.test(0))
+        self.assertFalse(pred0.test(1))
+        self.assertFalse(pred0.test(2))
+
+        pred1 = common_predicates.ExprPredicate("leq_pred", e, attr_inds, tols[1], [p1, p2], ["Can", "Sym"])
+        self.assertTrue(pred1.test(0))
+        self.assertTrue(pred1.test(1))
+        self.assertFalse(pred1.test(2))
+
+        pred2 = common_predicates.ExprPredicate("leq_pred", e, attr_inds, tols[2], [p1, p2], ["Can", "Sym"])
+        self.assertTrue(pred2.test(0))
+        self.assertTrue(pred2.test(1))
+        self.assertTrue(pred2.test(2))
+
+        ## its LEq, so increasing value for sym should make everything work
+        p2.value += 5
+        self.assertTrue(pred0.test(0))
+        self.assertTrue(pred0.test(1))
+        self.assertTrue(pred0.test(2))
+        self.assertTrue(pred1.test(0))
+        self.assertTrue(pred1.test(1))
+        self.assertTrue(pred1.test(2))
+        self.assertTrue(pred2.test(0))
+        self.assertTrue(pred2.test(1))
+        self.assertTrue(pred2.test(2))
+
+
+    def test_expr_at(self):
+        attrs = {"name": ["can"], "pose": ["undefined"], "_type": ["Can"]}
+        attr_types = {"name": str, "pose": Vector2d, "_type": str}
+        p1 = parameter.Object(attrs, attr_types)
+        attrs = {"name": ["target"], "pose": ["undefined"], "_type": ["Target"]}
+        attr_types = {"name": str, "pose": Vector2d, "_type": str}
+        p2 = parameter.Object(attrs, attr_types)
+
+        pred = common_predicates.At("testpred", [p1, p2], ["Can", "Target"])
         self.assertEqual(pred.get_type(), "At")
         self.assertFalse(pred.test(time=400))
-        p1.pose = np.array([[3, 4, 5], [6, 5, 7], [8, 9, 0]])
+        p1.pose = np.array([[3, 4, 5, 6], [6, 5, 7, 8]])
         # p2 doesn't have a value yet
         self.assertFalse(pred.test(time=400))
-        p2.pose = np.array([[3, 4, 5], [6, 5, 8], [8, 9, 1]])
+        p2.pose = np.array([[3, 4, 5, 7], [6, 5, 8, 7]])
         self.assertTrue(pred.is_concrete())
         with self.assertRaises(PredicateException) as cm:
-            pred.test(time=3)
-        self.assertEqual(cm.exception.message, "Out of range time for predicate 'testpred: (At can target sym)'.")
+            pred.test(time=4)
+        self.assertEqual(cm.exception.message, "Out of range time for predicate 'testpred: (At can target)'.")
         with self.assertRaises(PredicateException) as cm:
             pred.test(time=-1)
-        self.assertEqual(cm.exception.message, "Out of range time for predicate 'testpred: (At can target sym)'.")
+        self.assertEqual(cm.exception.message, "Out of range time for predicate 'testpred: (At can target)'.")
         self.assertTrue(pred.test(time=0))
         self.assertTrue(pred.test(time=1))
         self.assertFalse(pred.test(time=2))
+        self.assertFalse(pred.test(time=3))
+
+        attrs = {"name": ["sym"], "value": ["undefined"], "_type": ["Sym"]}
+        attr_types = {"name": str, "value": str, "_type": str}
+        p3 = parameter.Symbol(attrs, attr_types)
+        with self.assertRaises(PredicateException) as cm:
+            pred = common_predicates.At("testpred", [p1, p3], ["Can", "Target"])
+        self.assertEqual(cm.exception.message, "attribute type not supported")
 
     # TODO: test other predicates
