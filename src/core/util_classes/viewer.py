6aff4a59d0d9182cee48a797ef9eddb668a5d2e2
Simon0xzx <simon0xzx@gmail.com>
2016-07-06 22:10:50 -0700

    added draw and draw_traj functions and its tests

--- src/core/util_classes/viewer.py
+++ src/core/util_classes/viewer.py
@@ -1,4 +1,8 @@
 from IPython import embed as shell
+from openrave_body import OpenRAVEBody
+from openravepy import Environment
+from core.internal_repr.parameter import Object
+
 
 class Viewer(object):
     """
@@ -12,5 +16,36 @@ class GridWorldViewer(Viewer):
         pass
 
 class OpenRAVEViewer(Viewer):
+
+    def __init__(self):
+        self.env = Environment()
+        self.env.SetViewer('qtcoin')
+        self.param_to_rave_body = {}
+
     def initialize_from_workspace(self, workspace):
         pass
+
+    def draw(self, objList, t):
+        """
+        This function draws all the objects from the objList at timestep t
+
+        objList : list of parameters of type Object
+        t       : timestep of the trajectory
+        """
+        for obj in objList:
+            assert isinstance(obj, Object)
+            if obj not in self.param_to_rave_body:
+                self.param_to_rave_body[obj] = OpenRAVEBody(self.env, obj.name, obj.geom)
+            self.param_to_rave_body[obj].set_pose(obj.pose[:, t])
+
+    def draw_traj(self, objList, t_range):
+        """
+        This function draws the trajectory of objects from the objList
+
+        objList : list of parameters of type Object
+        t_range : range of timesteps to draw
+        """
+        for t in t_range:
+            for obj in objList:
+                rave_body = OpenRAVEBody(self.env, "{0}-{1}".format(obj.name, t), obj.geom)
+                rave_body.set_pose(obj.pose[:, t])
--- src/test/test_core/test_util_classes/test_viewer.py
+++ src/test/test_core/test_util_classes/test_viewer.py
@@ -1,12 +1,55 @@
 import unittest
 from core.util_classes import viewer
+from core.util_classes import circle
+from core.util_classes.matrix import Vector2d
+from core.internal_repr import parameter
+import numpy as np
+import time
+
 
 class TestViewer(unittest.TestCase):
-    # TODO
 
     def test_gridworldviewer(self):
         pass
 
-    def test_openraveviewer(self):
-        pass
+    def test_openraveviewer_draw(self):
+        attrs = {"geom": [1], "pose": [(3, 5)], "_type": ["Can"], "name": ["can0"]}
+        attr_types = {"geom": circle.GreenCircle, "pose": Vector2d, "_type": str, "name": str}
+        green_can = parameter.Object(attrs, attr_types)
 
+        attrs = {"geom": [1], "pose": [(2, 1)], "_type": ["Can"], "name": ["can1"]}
+        attr_types = {"geom": circle.BlueCircle, "pose": Vector2d, "_type": str, "name": str}
+        blue_can = parameter.Object(attrs, attr_types)
+
+        green_can.pose = np.array([[1,2,3,4,5],
+                                    [3,4,5,6,7]])
+        blue_can.pose = np.array([[3,4,5,6,7],
+                                [1,2,3,4,5]])
+        """
+        To check whether this works uncomment the following
+        """
+        # testViewer = viewer.OpenRAVEViewer()
+        # for t in range(5):
+        #     testViewer.draw([green_can, blue_can], t)
+        #     time.sleep(1)
+
+
+    def test_openraveviewer_draw_traj(self):
+        attrs = {"geom": [1], "pose": [(3, 5)], "_type": ["Can"], "name": ["can0"]}
+        attr_types = {"geom": circle.GreenCircle, "pose": Vector2d, "_type": str, "name": str}
+        green_can = parameter.Object(attrs, attr_types)
+
+        attrs = {"geom": [1], "pose": [(2, 1)], "_type": ["Can"], "name": ["can1"]}
+        attr_types = {"geom": circle.BlueCircle, "pose": Vector2d, "_type": str, "name": str}
+        blue_can = parameter.Object(attrs, attr_types)
+
+        green_can.pose = np.array([[1,2,3,4,5],
+                                    [3,4,5,6,7]])
+        blue_can.pose = np.array([[3,4,5,6,7],
+                                [1,2,3,4,5]])
+        """
+        To check whether this works uncomment the following
+        """
+        # testViewer = viewer.OpenRAVEViewer()
+        # testViewer.draw_traj([green_can, blue_can], range(5))
+        # import ipdb; ipdb.set_trace()
