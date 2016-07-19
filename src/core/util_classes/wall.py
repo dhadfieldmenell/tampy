2ac2c9f3c2a2d3d58e95be9231dd1b030f5c39e8
simon0xzx <simon0xzx@gmail.com>
2016-07-19 10:57:12 -0700

    created wall.py files

---
+++ src/core/util_classes/wall.py
@@ -0,0 +1,6 @@
+class Wall(object):
+    """
+    Defines geometry type of the Wall used in the Namo domain.
+    """
+    def __init__(self, wall_type):
+        self.wall_type = wall_type
---
+++ src/test/test_core/test_util_classes/test_wall.py
@@ -0,0 +1,5 @@
+from core.util_classes.wall import Wall
+import unittest
+
+class TestWall(unittest.TestCase):
+    pass
