376c8d4c7c0e9d06682820d6c5002e2300561900
Christopher Lin <c.l@berkeley.edu>
2016-09-22 18:32:52 -0700

    initial commit of admm solver
    implemented but untested, only _param_in_multiple_actions and
    _compute_shared_timesteps have been tested

---
+++ src/pma/admm_solver.py
@@ -0,0 +1,218 @@
+from sco.prob import Prob
+from sco.variable import Variable
+from sco.expr import BoundExpr, QuadExpr
+from sco.solver import Solver
+
+from core.util_classes.matrix import Vector
+
+from pma.ll_solver import NAMOSolver
+
+from collections import defaultdict
+from copy import deepcopy
+
+class ADMMHelper(object):
+    def __init__(self, consensus_dict, nonconsensus_dict):
+        recursive_default_dict = lambda: defaultdict(recursive_default_dict)
+        # lp = local param or param copy, lts = local timestep
+        self._lp_attr_lts_x_bar = recursive_default_dict()
+        self._lp_attr_lts_y = recursive_default_dict()
+        self._param_attr_ts_x_bar = recursive_default_dict()
+        for param, ts_to_triples in consensus_dict.iteritems():
+            for attr_name in param.__dict__.iterkeys():
+                attr_type = param.get_attr_type(attr_name)
+                if issubclass(attr_type, Vector):
+                    attr_val = getattr(param, attr_name)
+                    val = attr_val[:, ts]
+                    dim = attr_type.dim
+                    for ts, triples in ts_to_triple.iteritems():
+                        self._param_attr_ts_x_bar[param][attr_name][ts] = val
+                        for plan, param_copy, local_ts in triples:
+                            # adding entry in self._param_attr_ts_x_bar
+                            self._lp_attr_lts_x_bar[param_copy][attr_name][local_ts] = val
+                            self._lp_attr_lts_y[param_copy][attr_name][local_ts] = np.zeros(3)
+        """
+        param_ts_(plan, param_copy, local_ts) is good for updates
+        x_bar mapping param_copy, attr_name, local_ts
+        y mapping param_copy, attr_name, local_ts
+        """
+        self._consensus_dict = copy.deepcopy(consensus_dict)
+        self._nonconsensus_dict = copy.deepcopy(nonconsensus_dict)
+
+    def admm_step(self):
+        """
+        Updates x_bar and y
+        """
+        for param, ts_to_triple in self._consensus_dict.iteritems():
+            for attr_name in param.__dict__.iterkeys():
+                attr_type = param.get_attr_type(attr_name)
+                if issubclass(attr_type, Vector):
+                    attr_val = getattr(param, attr_name)
+                    val = attr_val[:, ts]
+                    dim = attr_type.dim
+                    for ts, triples in ts_to_triple.iteritems():
+                        assert len(triples) >= 2
+
+                        x_bar = np.zeros(dim)
+                        for plan, param_copy, local_ts in triples:
+                            x_bar += getattr(param_copy, attr_name)[:, local_ts]
+                        x_bar = x_bar/len(triples)
+                        self._param_attr_ts_x_bar[param][attr_name][ts] = x_bar
+
+                        for plan, param_copy, local_ts in triples:
+                            self._lp_attr_lts_x_bar[param_copy][attr_name][local_ts] = x_bar
+                            y_old = self._lp_attr_lts_y[param_copy][attr_name][local_ts]
+                            x = getattr(param_copy, attr_name)[:, local_ts]
+                            y = y_old + ro*(x_bar - x)
+                            self._lp_attr_lts_y[param_copy][attr_name][local_ts] = y
+
+    def update_params(self):
+        self._update_consensus()
+        self._update_nonconsensus()
+
+    def _update_consensus(self):
+        for param, attr_ts_x_bar in self._param_attr_ts_x_bar.iteritems():
+            for attr_name, ts_x_bar in attr_ts_x_bar.iteritems():
+                for ts, x_bar in ts_x_bar.iteritems():
+                    p_attr = getattr(param, attr_name)
+                    p_attr[:, ts] = x_bar
+
+    def _update_nonconsensus(self):
+        for param, ts_triple_dict in nonconsensus_dict.iteritems():
+            for attr_name in param.__dict__.iterkeys():
+                attr_type = param.get_attr_type(attr_name)
+                if issubclass(attr_type, Vector):
+                    for ts, triple in ts_triple_dict.iteritems():
+                        start, end = ts
+                        triple = (plan, param_copy, (s, e))
+                        p_attr = getattr(param, attr_name)
+                        p_attr[:, start:end+1] = getattr(param_copy, attr_name)[:, s:e+1]
+
+    def get_admm_exprs(self, local_param, ll_param, ro, trust_size=None):
+        admm_objs = []
+        # admm_cnts = []
+        attr_ts_x_bar_dict = self._lp_attr_lts_x_bar[local_param]
+        for attr_name, ts_x_bar_dict in attr_ts_x_bar_dict.iteritems():
+            for local_ts, x_bar in ts_x_bar_dict.iteritems():
+                yi = self._lp_attr_lts_y[local_param][attr_name][local_ts]
+
+                attr_val = getattr(local_param, attr_name)
+                K, T = attr_val.shape
+
+                # QuadExpr is 0.5*x^Tx + Ax + b
+                Q = ro*np.eye(K)
+                A = np.transpose(yi - 2*x_bar)
+                quad_expr = QuadExpr(Q, A, np.zeros((1,1)))
+
+                ll_attr_val = getattr(ll_param, attr_name)[:, local_ts]
+
+                param_ll_grb_vars = ll_attr_val.reshape((K, 1), order='F')
+                sco_var = Variable(param_ll_grb_vars, x_bar)
+                bexpr = BoundExpr(quad_expr, sco_var)
+                admm_objs.append(bexpr)
+
+        return admm_objs
+
+class NAMOADMMSolver(NAMOSolver):
+    def _param_in_multiple_actions(self, plan, param):
+        in_one_action = False
+        for action in plan.actions:
+            if param in action.params:
+                if in_one_action:
+                    return True
+                else:
+                    in_one_action = True
+        return False
+
+    def _compute_shared_timesteps(self, plan):
+        shared_timesteps = []
+        a_to_unshared_ranges = {}
+        if len(plan.actions) == 1:
+            a_to_unshared_ranges[plan.actions[0]] = (0, plan.horizon-1)
+        elif len(plan.actions) > 1:
+            for i, action in enumerate(plan.actions):
+                start, end = action.active_timesteps
+                if i > 0:
+                    shared_timesteps.append(start)
+
+                if i == 0:
+                    a_to_unshared_ranges[action] = (0, end-1)
+                elif i < len(plan.actions) - 1:
+                    a_to_unshared_ranges[action] = (start+1, end-1)
+                elif i == len(plan.actions) - 1:
+                    a_to_unshared_ranges[action] = (start+1, end)
+        return shared_timesteps, a_to_unshared_ranges
+
+    def _classify_variables(self, plan):
+        """
+        Returns a dictionary where the keys are parameters which contain
+        consensus variables and the values are the timesteps for the consensus
+        variables.
+        """
+        consensus_dict = {}
+        nonconsensus_dict = {}
+        shared_timesteps, unshared_ranges = self._compute_shared_timesteps(plan)
+        shared_ts_dict = {t: [] for t in shared_timesteps}
+        unshared_ts_dict = {r: None for r in unshared_ranges}
+        for param in plan.params:
+            if param.is_symbol():
+                # loop through each symbolic parameter and check if its in multiple actions
+                if self._param_in_multiple_actions(plan, param):
+                    consensus_dict[param] = {0: []}
+                else:
+                    assert param not in nonconsensus_dict
+                    nonconsensus_dict[param] = {(0,0): None}
+            else:
+                # object parameters with shared timesteps are consensus variables
+                consensus_dict[param] = shared_ts_dict.copy()
+                nonconsensus_dict[param] = unshared_ts_dict.copy()
+        return consensus_dict, nonconsensus_dict
+
+    # http://stanford.edu/~boyd/papers/pdf/admm_distr_stats.pdf page 48
+    # Global variable consensus optimization
+    def admm_solve(self, plan, callback, verbose=False):
+        penalty_coeff = 1e2
+        consensus_trust = 0.1
+        eps_primal = 1e-3
+        eps_dual = 1e-3
+        ro = 300
+        MAX_ITERS = 5
+        consensus_dict, nonconsensus_dict = self._classify_variables(plan)
+
+        solv = Solver()
+        solv.initial_penalty_coeff = self.init_penalty_coeff
+        action_solvers = self._setup_action_solvers(plan)
+        action_plans = plan.get_action_plans(consensus_dict, nonconsensus_dict)
+        admm_help = ADMMHelper(consensus_dict, nonconsensus_dict)
+        for i in range(MAX_ITERS):
+            for action_plan in action_plans:
+                self._solve_admm_subproblem(action_plan, admm_help)
+
+            # compute x_bar and y's
+            admm_help.admm_step()
+            # update variable values in the consensus variable optimization
+            admm_help.update_params()
+
+    def _solve_admm_subproblem(self, plan, admm_help, ro):
+        active_ts = (0, plan.horizon-1)
+        model = grb.Model()
+        model.params.OutputFlag = 0
+        namo_solver = NAMOSolver()
+        namo_solver._prob = Prob(model, callback=callback)
+        # param_to_ll_old = self._param_to_ll.copy()
+        namo_solver._spawn_parameter_to_ll_mapping(model, plan, active_ts)
+        model.update()
+
+        obj_bexprs = namo_solver._get_trajopt_obj(plan, active_ts)
+        cnt_bexprs = []
+        for param in plan.params:
+            ll_param = namo_solver._param_to_ll[param]
+            admm_objs = admm_help.get_admm_exprs(param, ll_param, ro)
+            obj_bexprs.extend(admm_objs)
+        namo_solver._add_obj_bexprs(obj_bexprs)
+
+        solv = Solver()
+        solv.initial_penalty_coeff = namo_solver.init_penalty_coeff
+        success = solv.solve(namo_solver._prob, method='penalty_sqp', tol=tol, verbose=verbose)
+        namo_solver._update_ll_params()
+        namo_solver._failed_groups = namo_solver._prob.nonconverged_groups
+        return success
---
+++ src/test/test_pma/test_admm_solver.py
@@ -0,0 +1,87 @@
+import unittest
+import time, main
+from pma import hl_solver
+from pma.admm_solver import ADMMHelper, NAMOADMMSolver
+from core.parsing import parse_domain_config
+from core.parsing import parse_problem_config
+
+class TestADMMSolver(unittest.TestCase):
+    def setUp(self):
+
+        domain_fname = '../domains/namo_domain/namo.domain'
+        d_c = main.parse_file_to_dict(domain_fname)
+        domain = parse_domain_config.ParseDomainConfig.parse(d_c)
+        hls = hl_solver.FFSolver(d_c)
+
+        def get_plan(p_fname, plan_str=None):
+            p_c = main.parse_file_to_dict(p_fname)
+            problem = parse_problem_config.ParseProblemConfig.parse(p_c, domain)
+            abs_problem = hls.translate_problem(problem)
+            if plan_str is not None:
+                return hls.get_plan(plan_str, domain, problem)
+            return hls.solve(abs_problem, domain, problem)
+
+        self.move_no_obs = get_plan('../domains/namo_domain/namo_probs/move_no_obs.prob')
+        self.move_w_obs = get_plan('../domains/namo_domain/namo_probs/move_w_obs.prob')
+        self.move_grasp = get_plan('../domains/namo_domain/namo_probs/move_grasp.prob')
+        self.move_grasp_moveholding = get_plan('../domains/namo_domain/namo_probs/moveholding.prob')
+        self.place = get_plan('../domains/namo_domain/namo_probs/place.prob')
+        self.putaway = get_plan('../domains/namo_domain/namo_probs/putaway.prob')
+        self.putaway3 = get_plan('../domains/namo_domain/namo_probs/putaway3.prob')
+
+        self.putaway2 = get_plan('../domains/namo_domain/namo_probs/putaway2.prob',
+                ['0: MOVETO PR2 ROBOT_INIT_POSE PDP_TARGET2',
+                '1: GRASP PR2 CAN0 TARGET0 PDP_TARGET2 PDP_TARGET0 GRASP0',
+                '2: MOVETOHOLDING PR2 PDP_TARGET0 PDP_TARGET2 CAN0 GRASP0',
+                '3: PUTDOWN PR2 CAN0 TARGET2 PDP_TARGET2 ROBOT_END_POSE GRASP0'])
+
+
+    def test_param_in_multiple_actions(self):
+        nas = NAMOADMMSolver()
+        plan = self.move_no_obs
+        param = plan.params['robot_init_pose']
+        self.assertFalse(nas._param_in_multiple_actions(plan, param))
+        param = plan.params['robot_end_pose']
+        self.assertFalse(nas._param_in_multiple_actions(plan, param))
+
+        plan = self.move_grasp_moveholding
+        param = plan.params['robot_init_pose']
+        self.assertFalse(nas._param_in_multiple_actions(plan, param))
+        param = plan.params['target1']
+        self.assertFalse(nas._param_in_multiple_actions(plan, param))
+        param = plan.params['robot_end_pose']
+        self.assertTrue(nas._param_in_multiple_actions(plan, param))
+        param = plan.params['grasp0']
+        self.assertTrue(nas._param_in_multiple_actions(plan, param))
+        param = plan.params['can1']
+        self.assertTrue(nas._param_in_multiple_actions(plan, param))
+
+    def test_compute_shared_timesteps(self):
+        nas = NAMOADMMSolver()
+        plan = self.move_no_obs
+        shared_timesteps, a_to_unshared_ranges = nas._compute_shared_timesteps(plan)
+        self.assertTrue(len(shared_timesteps) == 0)
+        action = plan.actions[0]
+        self.assertTrue(action in a_to_unshared_ranges)
+        self.assertTrue(a_to_unshared_ranges[action] == (0, plan.horizon-1))
+
+        plan = self.move_grasp_moveholding
+        shared_timesteps, a_to_unshared_ranges = nas._compute_shared_timesteps(plan)
+        self.assertTrue(len(plan.actions) == 3)
+        self.assertTrue(len(shared_timesteps) == 2)
+        self.assertTrue(plan.actions[0].active_timesteps[1] in shared_timesteps)
+        self.assertTrue(plan.actions[1].active_timesteps[1] in shared_timesteps)
+
+        for i, action in enumerate(plan.actions):
+            start, end = action.active_timesteps
+            if i == 0:
+                self.assertTrue(a_to_unshared_ranges[action] == (start, end-1))
+            elif i == len(plan.actions) - 1:
+                self.assertTrue(a_to_unshared_ranges[action] == (start+1, end))
+            else:
+                self.assertTrue(a_to_unshared_ranges[action] == (start+1, end-1))
+
+    def test_classify_variables(self):
+        pass
+        # test that all variables or nonconsensus for self.move_no_obs
+        # test that some variables are consensus for self.move_grasp_moveholding
