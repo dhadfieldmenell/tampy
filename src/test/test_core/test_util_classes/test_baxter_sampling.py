6de7682bb58c4c70dba436727fe432c473a6eee3
simon0xzx <simon0xzx@gmail.com>
2017-01-25 22:35:52 -0800

    Added test on resample rcollides

--- src/test/test_core/test_util_classes/test_baxter_sampling.py
+++ src/test/test_core/test_util_classes/test_baxter_sampling.py
@@ -2,6 +2,7 @@ import unittest
 import time
 import main
 import numpy as np
+from pma import hl_solver
 from core.parsing import parse_domain_config, parse_problem_config
 from core.internal_repr import parameter
 from core.util_classes import box, matrix, baxter_predicates, baxter_sampling
@@ -19,7 +20,9 @@ def load_environment(domain_file, problem_file):
     p_c = main.parse_file_to_dict(p_fname)
     problem = parse_problem_config.ParseProblemConfig.parse(p_c, domain)
     params = problem.init_state.params
-    return domain, problem, params
+    hls = hl_solver.FFSolver(d_c)
+    plan = hls.get_plan(['0: GRASP BAXTER CAN0 TARGET0 PDP_TARGET0 EE_TARGET0 ROBOT_END_POSE'], domain, problem)
+    return domain, problem, params, plan
 
 def planing(env, robot, params, traj, planner):
     t0 = time.time()
@@ -39,7 +42,7 @@ def planing(env, robot, params, traj, planner):
 class TestBaxterSampling(unittest.TestCase):
 
     def test_resample_ee_reachable(self):
-        domain, problem, params = load_environment("../domains/baxter_domain/baxter.domain", "../domains/baxter_domain/baxter_probs/grasp_1234_1.prob")
+        domain, problem, params, plan = load_environment("../domains/baxter_domain/baxter.domain", "../domains/baxter_domain/baxter_probs/grasp_1234_1.prob")
 
         env = problem.env
         objLst = [i[1] for i in params.items() if not i[1].is_symbol()]
@@ -74,7 +77,7 @@ class TestBaxterSampling(unittest.TestCase):
         # import ipdb; ipdb.set_trace()
 
     def test_resampling_rrt(self):
-        domain, problem, params = load_environment("../domains/baxter_domain/baxter.domain", "../domains/baxter_domain/baxter_probs/grasp_1234_1.prob")
+        domain, problem, params, plan = load_environment("../domains/baxter_domain/baxter.domain", "../domains/baxter_domain/baxter_probs/grasp_1234_1.prob")
 
         env = problem.env
         objLst = [i[1] for i in params.items() if not i[1].is_symbol()]
@@ -95,3 +98,31 @@ class TestBaxterSampling(unittest.TestCase):
         val, attr_inds = pred.resample(None, 8, None)
 
         import ipdb; ipdb.set_trace()
+
+    def test_resample_r_collides(self):
+        domain, problem, params, plan = load_environment("../domains/baxter_domain/baxter.domain", "../domains/baxter_domain/baxter_probs/baxter_test_env.prob")
+        viewer = OpenRAVEViewer.create_viewer(plan.env)
+        viewer.draw_plan_ts(plan, 0)
+        baxter = plan.params['baxter']
+        obstacle = plan.params['table']
+        rcollides_pred = baxter_predicates.BaxterRCollides("test_rcollides", [baxter, obstacle], ["Robot", "Obstacle"], plan.env)
+        start_pose = np.array([ 0.4,  0.8,  1. ,  0.4,  0. ,  0. ,  0. ])
+        end_pose = np.array([ 0.5  , -0.881,  0.814,  1.669, -2.672,  0.864,  2.308])
+        traj = []
+        for i in range(7):
+            traj.append(np.linspace(start_pose[i], end_pose[i], 40, endpoint=True))
+        traj = np.r_[traj]
+        baxter.rArmPose = traj
+        baxter.lArmPose = np.repeat(baxter.lArmPose[:, 0].reshape((7, 1)), 40, axis = 1)
+        baxter.rGripper = np.repeat(baxter.rGripper[:, 0].reshape((1, 1)), 40, axis = 1)
+        baxter.lGripper = np.repeat(baxter.lGripper[:, 0].reshape((1, 1)), 40, axis = 1)
+        baxter.pose = np.repeat(baxter.pose[:, 0].reshape((1, 1)), 40, axis = 1)
+        obstacle.pose = np.repeat(obstacle.pose[:, 0].reshape((3, 1)), 40, axis = 1)
+        obstacle.rotation = np.repeat(obstacle.rotation[:, 0].reshape((3, 1)), 40, axis = 1)
+        for i in range(1, 30):
+            self.assertTrue(rcollides_pred.test(i))
+
+        rcollides_pred.resample(False, 1, plan)
+        self.assertFalse(rcollides_pred.test(1))
+        import ipdb; ipdb.set_trace()
+        rcollides_pred.resample(False, 2, plan)
