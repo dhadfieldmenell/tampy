7be1613fc7ae3bdf203cac341a15276234c5d6ba
simon0xzx <simon0xzx@gmail.com>
2017-03-27 09:22:54 -0700

    inplemented training and sampling prototype for learning class

--- src/core/util_classes/learning.py
+++ src/core/util_classes/learning.py
@@ -21,8 +21,14 @@ class Learner(object):
         raise NotImplementedError
 
 class PostLearner(Learner):
+    """
+    User defined element:
+    A distribution over the plan,
+    feature functions for for the parameters,
+    what parameters are needed to be trained.
+    """
 
-    def __init__(self, file_name, train_size, episode_size, space = "CONFIG"):
+    def __init__(self, arg_dict, file_name, space = "CONFIG"):
         """
         Constructor will first try to load the feature value theta
         If no useful information is loaded, we will mark this learner
@@ -44,8 +50,12 @@ class PostLearner(Learner):
         except:
             print "File not found, please train this Learner."
 
-        self.train_size = train_size
-        self.episode_size = episode_size
+        self.train_size = arg_dict['train_size']
+        self.episode_size = arg_dict['episode_size']
+        self.solver = arg_dict['solver']
+        self.sample_iter = arg_dict['sample_iter']
+        self.sample_burn = arg_dict['sample_burn']
+        self.sample_thin = arg_dict['sample_thin']
 
     def sample_space(self):
         return self._sample_space
@@ -78,7 +88,6 @@ class PostLearner(Learner):
                 attr_theta = param_group.create_dataset(attr, attr_dict[attr].shape)
                 attr_theta[...] = attr_dict[attr]
 
-
     def change_space(self, new_space):
         """
         This function allows user to switch space to sample from.
@@ -110,10 +119,34 @@ class PostLearner(Learner):
     def train_work(self, domain, problem, feature_fun):
         pass
 
-    def train_config(self, domain, problem, feature_fun):
-        pass
+    def train_config(self, problems, feature_fun, param_dict, step_size = 0.05):
+        """
+            Solver needs to provide a method train_sample
+            that takes in a problem, episode_size, and feature_fun
+            and returns each sample episode's feature vector (List(Dict:param->feature))
+            and its reward (List)
+        """
 
-    def sample(self, state, feature_fun):
+        for prob in problems:
+            features, reward = solver.train_sample(porb, self.episode_size, feature_fun)
+            R = np.sum(reward)
+            for param in param_dict:
+                for attr in param:
+                    dist_list = np.array([np.exp(self.theta[param][attr].dot(fea[param][attr])) \
+                                                            for fea in features])
+                    dist = dist_list/np.sum(dist_list)
+
+                    expected = 0
+                    for index in range(self.episode_size):
+                        expected += features[i][param][attr]*dist[i]
+
+                    grad = 0
+                    for i in range(self.episode_size):
+                        grad += features[i][param][attr] - expected
+                    gradient = R / float(self.episode_size) * grad
+                    self.theta[param][attr] = self.theta[param][attr] + step_size* gradient
+
+    def sample(self, state, param_dict, feature_fun):
         if self._sample_space == "CONFIG":
             self.sample_config(state, feature_fun)
         elif self._sample_space == "WORK":
@@ -121,33 +154,48 @@ class PostLearner(Learner):
         else:
             assert "Invalid Sample Space Specified"
 
-    def sample_work(self, state, feature_fun):
+    def sample_work(self, state, param_dict, feature_fun):
         pass
 
-    def sample_config(self, state, feature_fun):
+    def sample_config(self, state, param_dict, feature_fun):
         """
         Given a feature_mapping:
         (dict: param_name->(attr_dict: attr->value))
         sample according to Giab Distribution biased by parameter self.theta.
         Return dictionary mapping param_name to sampled value.
         """
-        #TODO Currently not exactly correct
-        config_value = {}
-        for param in self.theta:
-            attr_dict = self.theta[param]
-            for attr in attr_dict:
-                feature = feature_fun[param][attr]
-                theta = self.theta[param][attr]
-                config_value[param][attr] = self.sample_helper(theta, feature)
-        return config_value
+        def model(param, attr, alpha):
+            np.exp(self.theta[param][attr].T.dot(feature_fun(alpha)))
 
-    def sample_helper(self, theta, feature):
-        sample_list = []
-        for i in range(self.episode_size):
-            sample_list.append(np.exp(theta.dot(feature)))
-        sample_list = np.array(sample_list) / np.linalg.norm(sample_list)
-        accumulate = [0]
-        for sample in sample_list:
-            accumulate.append(accumulate[-1] + sample)
-        beta = np.random.uniform()
-        #TODO not yet finished#
+        return self.metropolis_hasting(param_dict, feature_fun, model, self.sample_iter, self.sample_burn, self.sample_thin)
+
+    def metropolis_hasting(self, param_dict, feature_fun, model, iteration, burn, thin):
+        sample_dict = {}
+        for param in param_dict:
+            for attr in param_dict[param]:
+                # initial guess for alpha as array.
+                old_alpha = np.multiply(np.random.sample(param_dict[param][attr].shape[0]), param_dict[param][attr][:,1] - param_dict[param][attr][:,0]) + param_dict[param][attr][:,0]
+                samples = [old_alpha]
+                new_alpha = np.zeros((len(old_alpha),))
+                # define stepsize of MCMC.
+                stepsizes = [0.005] * len(old_alpha)  # array of stepsizes
+                accepted  = 0.0
+                # Metropolis-Hastings with 10,000 iterations.
+                for n in range(iteration):
+                    likelihood = model(param, attr, old_alpha)
+                    accumulated_sum = likelihood
+                    for i in range(len(old_alpha)):
+                        # Use stepsize provided for every dimension.
+                        new_alpha[i] = random.gauss(old_alpha[i], stepsizes[i])
+                    # Suggest new candidate from Gaussian proposal distribution.
+                    likelihood = model(param, attr, new_alpha)
+
+                    # Accept new candidate in Monte-Carlo fashing.
+                    accept_prob = min([1.,new_likelihood/likelihood])
+                    u = random.uniform(0.0,1.0)
+                    if u < aprob:
+                        old_alpha = new_alpha
+                        samples.append(new_alpha)
+                result_sampling = samples[burn::thin]
+                index = np.random.randint(0, len(result_sampling), 1)
+                sample_dict[param][attr] = result_sampling[index]
--- src/test/test_core/test_util_classes/test_learning.py
+++ src/test/test_core/test_util_classes/test_learning.py
@@ -7,7 +7,8 @@ class TestLearner(unittest.TestCase):
     def test_initialize(self):
         if os.path.isfile("test_learner.hdf5"):
             os.remove("test_learner.hdf5")
-        learner = PostLearner("test_learner", "CONFIG")
+        arg_dict = {'train_size': 10, 'episode_size': 5, 'solver': None, 'sample_iter': 1000, 'sample_burn': 250, 'sample_thin': 3}
+        learner = PostLearner(arg_dict, "test_learner", "CONFIG")
         self.assertEqual(learner.store_file, "test_learner.hdf5")
         self.assertEqual(learner.sample_space(), "CONFIG")
         self.assertEqual(learner.theta, None)
@@ -46,10 +47,36 @@ class TestLearner(unittest.TestCase):
                 theta_value = data[param][attr]
                 self.assertEqual(np.allclose(data_value, theta_value), True)
         hdf5.close()
-        learner2 = PostLearner("test_learner", "CONFIG")
+        learner2 = PostLearner(arg_dict, "test_learner", "CONFIG")
         self.assertTrue(learner2.theta != None)
         self.assertTrue(learner2.trained)
         os.remove("test_learner.hdf5")
+
+    def test_sampling(self):
+        arg_dict = {'train_size': 10, 'episode_size': 5, 'solver': None, 'sample_iter': 1000, 'sample_burn': 250, 'sample_thin': 3}
+        learn = PostLearner(arg_dict, "test_learner", "CONFIG")
+        data = {"robot":{"lArmPose": np.ones((7,)),
+                         "rArmPose": np.ones((7,)),
+                         "pose": np.ones((1,)),
+                         "lGripper": np.ones((1,)),
+                         "rGripper": np.ones((1,))
+                         }
+               }
+        learn.theta = data
+        def feature_fun(x):
+            return x
+        param_dict = {"robot":{"lArmPose": np.hstack([np.zeros((7,1)), 10*np.ones((7,1))]),
+                         "rArmPose": np.hstack([np.zeros((7,1)), 10*np.ones((7,1))]),
+                         "pose": np.array([[0, 10]]),
+                         "lGripper": np.array([[0, 10]]),
+                         "rGripper": np.array([[0, 10]])
+                         }
+               }
+        sample = learn.sample_config(None, param_dict, feature_fun)
+        import ipdb; ipdb.set_trace()
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
