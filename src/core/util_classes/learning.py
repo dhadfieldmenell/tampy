23c0f0e75de101d5f8200e6b36d5226e14fb417c
simon0xzx <simon0xzx@gmail.com>
2017-03-27 18:27:34 -0700

    Finished Metroplis Hasting Algorithm and corresponding test

--- src/core/util_classes/learning.py
+++ src/core/util_classes/learning.py
@@ -2,6 +2,7 @@ import h5py
 import os.path as path
 from IPython import embed as shell
 import numpy as np
+import math, random
 
 class Learner(object):
     """
@@ -56,6 +57,7 @@ class PostLearner(Learner):
         self.sample_iter = arg_dict['sample_iter']
         self.sample_burn = arg_dict['sample_burn']
         self.sample_thin = arg_dict['sample_thin']
+        self.train_stepsize = arg_dict['train_stepsize']
 
     def sample_space(self):
         return self._sample_space
@@ -108,18 +110,18 @@ class PostLearner(Learner):
             self._sample_space = "UNDEFINED"
             assert "Invalid sample space. new sapce has to be either CONFIG for configuration space or WORK for work space"
 
-    def train(self, domain, problem, feature_fun):
+    def train(self, problem, feature_fun, param_dict):
         if self._sample_space == "CONFIG":
-            self.train_config(domain, problem, feature_fun)
+            self.train_config(problem, feature_fun, param_dict)
         elif self._sample_space == "WORK":
-            self.train_work(domain, problem, feature_fun)
+            self.train_work(problem, feature_fun, param_dict)
         else:
             assert "Invalid Sample Space Specified"
 
-    def train_work(self, domain, problem, feature_fun):
+    def train_work(self, problem, feature_fun, param_dict):
         pass
 
-    def train_config(self, problems, feature_fun, param_dict, step_size = 0.05):
+    def train_config(self, problems, feature_fun, param_dict):
         """
             Solver needs to provide a method train_sample
             that takes in a problem, episode_size, and feature_fun
@@ -132,8 +134,7 @@ class PostLearner(Learner):
             R = np.sum(reward)
             for param in param_dict:
                 for attr in param:
-                    dist_list = np.array([np.exp(self.theta[param][attr].dot(fea[param][attr])) \
-                                                            for fea in features])
+                    dist_list = np.array([np.exp(self.theta[param][attr].dot(fea[param][attr])) for fea in features])
                     dist = dist_list/np.sum(dist_list)
 
                     expected = 0
@@ -144,58 +145,65 @@ class PostLearner(Learner):
                     for i in range(self.episode_size):
                         grad += features[i][param][attr] - expected
                     gradient = R / float(self.episode_size) * grad
-                    self.theta[param][attr] = self.theta[param][attr] + step_size* gradient
+                    self.theta[param][attr] = self.theta[param][attr] + self.train_stepsize* gradient
+                    import ipdb; ipdb.set_trace()
 
-    def sample(self, state, param_dict, feature_fun):
+    def sample(self, param_dict, feature_fun):
         if self._sample_space == "CONFIG":
-            self.sample_config(state, feature_fun)
+            self.sample_config(param_dict, feature_fun)
         elif self._sample_space == "WORK":
-            self.sample_work(state, feature_fun)
+            self.sample_work(param_dict, feature_fun)
         else:
             assert "Invalid Sample Space Specified"
 
-    def sample_work(self, state, param_dict, feature_fun):
+    def sample_work(self, param_dict, feature_fun):
         pass
 
-    def sample_config(self, state, param_dict, feature_fun):
+    def sample_config(self, param_dict, feature_fun):
         """
         Given a feature_mapping:
         (dict: param_name->(attr_dict: attr->value))
         sample according to Giab Distribution biased by parameter self.theta.
         Return dictionary mapping param_name to sampled value.
         """
-        def model(param, attr, alpha):
-            np.exp(self.theta[param][attr].T.dot(feature_fun(alpha)))
 
-        return self.metropolis_hasting(param_dict, feature_fun, model, self.sample_iter, self.sample_burn, self.sample_thin)
-
-    def metropolis_hasting(self, param_dict, feature_fun, model, iteration, burn, thin):
-        sample_dict = {}
         for param in param_dict:
+            sample_dict[param] = {}
             for attr in param_dict[param]:
-                # initial guess for alpha as array.
-                old_alpha = np.multiply(np.random.sample(param_dict[param][attr].shape[0]), param_dict[param][attr][:,1] - param_dict[param][attr][:,0]) + param_dict[param][attr][:,0]
-                samples = [old_alpha]
-                new_alpha = np.zeros((len(old_alpha),))
-                # define stepsize of MCMC.
-                stepsizes = [0.005] * len(old_alpha)  # array of stepsizes
-                accepted  = 0.0
-                # Metropolis-Hastings with 10,000 iterations.
-                for n in range(iteration):
-                    likelihood = model(param, attr, old_alpha)
-                    accumulated_sum = likelihood
-                    for i in range(len(old_alpha)):
-                        # Use stepsize provided for every dimension.
-                        new_alpha[i] = random.gauss(old_alpha[i], stepsizes[i])
-                    # Suggest new candidate from Gaussian proposal distribution.
-                    likelihood = model(param, attr, new_alpha)
+                def model(alpha):
+                    return np.exp(self.theta[param][attr].T.dot(feature_fun(alpha)))
 
-                    # Accept new candidate in Monte-Carlo fashing.
-                    accept_prob = min([1.,new_likelihood/likelihood])
-                    u = random.uniform(0.0,1.0)
-                    if u < aprob:
-                        old_alpha = new_alpha
-                        samples.append(new_alpha)
-                result_sampling = samples[burn::thin]
-                index = np.random.randint(0, len(result_sampling), 1)
+                def sample_step(prev):
+                    return np.random.normal(0, 1, prev)
+
+                result_sampling =  self.metropolis_hasting(param_dict[param][attr], feature_fun, model, sample_step)
+                index = np.random.randint(0, len(result_sampling))
                 sample_dict[param][attr] = result_sampling[index]
+        return sample_dict
+
+    def metropolis_hasting(self, boundary, feature_fun, model, sample_step):
+        """
+            This function implements Metropolis Hasting Algorithm for sampling.
+            Arg:
+                Boundary: A kx2 matrix defines lower bound(first column) and upper bound(last column) of the sample you wish to draw.
+                Feature_fun: Defines a mapping between sample and it's coresponding feature vector.
+                Model: is pdf from which the sample is draw from
+                Sample_step: A function that takes in previous data point and sample the next.(should draw sample from the same distribution as Model)
+        """
+        # initial guess for alpha as array.
+        old_alpha = np.multiply(np.random.sample(boundary.shape[0]), boundary[:,1] - boundary[:,0]) + boundary[:,0]
+        samples = [old_alpha]
+        new_alpha = np.zeros((len(old_alpha),))
+        # Metropolis-Hastings with 10,000 iterations.
+        for n in range(self.sample_iter):
+            likelihood = model(old_alpha)
+            # This new sample should be draw from the same distribution as model
+            new_alpha = sample_step(old_alpha)
+            new_likelihood = model(new_alpha)
+            # Accept new candidate in Monte-Carlo fashing.
+            accept_prob = min([1.,new_likelihood/float(likelihood)])
+            u = random.uniform(0.0,1.0)
+            if u < accept_prob:
+                old_alpha = new_alpha
+                samples.append(new_alpha)
+        return np.array(samples[self.sample_burn::self.sample_thin])
--- src/test/test_core/test_util_classes/test_learning.py
+++ src/test/test_core/test_util_classes/test_learning.py
@@ -1,13 +1,14 @@
 import unittest, os, h5py
 import numpy as np
 from core.util_classes.learning import PostLearner
-
+from math import *
+import matplotlib.pylab as plt
 class TestLearner(unittest.TestCase):
 
-    def test_initialize(self):
+    def test_initialize_file_storage(self):
         if os.path.isfile("test_learner.hdf5"):
             os.remove("test_learner.hdf5")
-        arg_dict = {'train_size': 10, 'episode_size': 5, 'solver': None, 'sample_iter': 1000, 'sample_burn': 250, 'sample_thin': 3}
+        arg_dict = {'train_size': 10, 'episode_size': 5, 'solver': None, 'train_stepsize': 0.05, 'sample_iter': 1000, 'sample_burn': 250, 'sample_thin': 3}
         learner = PostLearner(arg_dict, "test_learner", "CONFIG")
         self.assertEqual(learner.store_file, "test_learner.hdf5")
         self.assertEqual(learner.sample_space(), "CONFIG")
@@ -52,28 +53,51 @@ class TestLearner(unittest.TestCase):
         self.assertTrue(learner2.trained)
         os.remove("test_learner.hdf5")
 
-    def test_sampling(self):
-        arg_dict = {'train_size': 10, 'episode_size': 5, 'solver': None, 'sample_iter': 1000, 'sample_burn': 250, 'sample_thin': 3}
+    def test_metropolis_hasting(self):
+        # Testing with single variable Normal Distribution
+        arg_dict = {'train_size': 10, 'episode_size': 5, 'solver': None, 'train_stepsize': 0.05, 'sample_iter': 50000, 'sample_burn': 500, 'sample_thin': 2}
         learn = PostLearner(arg_dict, "test_learner", "CONFIG")
-        data = {"robot":{"lArmPose": np.ones((7,)),
-                         "rArmPose": np.ones((7,)),
-                         "pose": np.ones((1,)),
-                         "lGripper": np.ones((1,)),
-                         "rGripper": np.ones((1,))
-                         }
-               }
+        data = {"dummy":{"x": np.array([[1]])}}
         learn.theta = data
         def feature_fun(x):
             return x
-        param_dict = {"robot":{"lArmPose": np.hstack([np.zeros((7,1)), 10*np.ones((7,1))]),
-                         "rArmPose": np.hstack([np.zeros((7,1)), 10*np.ones((7,1))]),
-                         "pose": np.array([[0, 10]]),
-                         "lGripper": np.array([[0, 10]]),
-                         "rGripper": np.array([[0, 10]])
-                         }
-               }
-        sample = learn.sample_config(None, param_dict, feature_fun)
-        import ipdb; ipdb.set_trace()
+        def sdnorm(z):
+            """
+            Standard normal pdf
+            """
+            return np.exp(-(z-5)*(z-5)/2*(1)**2)/sqrt(2*pi*(1)**2)
+
+        def sample_step(old_alpha):
+            return np.random.logistic(old_alpha, 3, len(old_alpha))
+
+        boundary = np.array([[-3, 3]])
+        samples = learn.metropolis_hasting(boundary, feature_fun, sdnorm, sample_step)
+
+        x = np.arange(2,8,.1)
+        y = [sdnorm(i) for i in x]
+        plt.subplot(211)
+        plt.title('Metropolis-Hastings')
+        plt.plot(samples)
+        plt.subplot(212)
+
+        plt.hist(samples[1000::3], bins=30,normed=1)
+        plt.plot(x,y,'ro')
+        plt.ylabel('Frequency')
+        plt.xlabel('x')
+        plt.legend(('PDF','Samples'))
+        plt.show()
+
+    def test_training(self):
+        arg_dict = {'train_size': 10, 'episode_size': 5, 'solver': None, 'train_stepsize': 0.05, 'sample_iter': 10000, 'sample_burn': 500, 'sample_thin': 2}
+        learn = PostLearner(arg_dict, "test_learner", "CONFIG")
+        data = {"dummy":{"x": np.array([[1]])}}
+        learn.theta = data
+
+
+    def test_sampling(self):
+        pass
+
+
 
 
 
