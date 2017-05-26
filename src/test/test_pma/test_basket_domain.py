64b287d46b7f47bc5403e2326406e404347ac7aa
simon0xzx <simon0xzx@gmail.com>
2017-05-25 20:21:27 -0700

    Adding test to find initial positions

--- src/test/test_pma/test_basket_domain.py
+++ src/test/test_pma/test_basket_domain.py
@@ -2,7 +2,9 @@ import numpy as np
 import unittest, time, main
 from pma import hl_solver, robot_ll_solver
 from core.parsing import parse_domain_config, parse_problem_config
+from core.util_classes.openrave_body import OpenRAVEBody
 from core.util_classes.viewer import OpenRAVEViewer
+from core.util_classes.param_setup import ParamSetup
 from core.util_classes.plan_hdf5_serialization import PlanSerializer, PlanDeserializer
 from ros_interface import action_execution
 
@@ -14,10 +16,10 @@ class TestBasketDomain(unittest.TestCase):
         domain = parse_domain_config.ParseDomainConfig.parse(d_c)
         hls = hl_solver.FFSolver(d_c)
         print "loading basket problem..."
-        p_c = main.parse_file_to_dict('../domains/baxter_domain/baxter_probs/basket_env.prob')
+        p_c = main.parse_file_to_dict('../domains/baxter_domain/baxter_probs/basket_move.prob')
         problem = parse_problem_config.ParseProblemConfig.parse(p_c, domain)
-        plan_str = ['0: BASKET_GRASP BAXTER BASKET TARGET ROBOT_INIT_POSE EE_LEFT EE_RIGHT ROBOT_END_POSE',
-                    '1: BASKET_PUTDOWN BAXTER BASKET TARGET ROBOT_INIT_POSE EE_LEFT EE_RIGHT ROBOT_END_POSE']
+        plan_str = ['0: BASKET_GRASP BAXTER BASKET INIT_TARGET ROBOT_INIT_POSE GRASP_EE_LEFT GRASP_EE_RIGHT PICKUP_POSE',
+                    '1: BASKET_PUTDOWN BAXTER BASKET END_TARGET PICKUP_POSE PUTDOWN_EE_LEFT PUTDOWN_EE_RIGHT ROBOT_END_POSE']
         plan = hls.get_plan(plan_str, domain, problem)
 
         print "solving basket domain problem..."
@@ -47,6 +49,46 @@ class TestBasketDomain(unittest.TestCase):
         # print "executing plan in Baxter..."
         # for act in plan.actions:
         #     action_execution.execute_action(act)
+
+
+    def test_basket_position(self):
+
+        def load_environment(domain_file, problem_file):
+            domain_fname = domain_file
+            d_c = main.parse_file_to_dict(domain_fname)
+            domain = parse_domain_config.ParseDomainConfig.parse(d_c)
+            p_fname = problem_file
+            p_c = main.parse_file_to_dict(p_fname)
+            problem = parse_problem_config.ParseProblemConfig.parse(p_c, domain)
+            params = problem.init_state.params
+            return domain, problem, params
+
+        domain, problem, params = load_environment('../domains/baxter_domain/baxter_basket_grasp.domain',
+                       '../domains/baxter_domain/baxter_probs/basket_move.prob')
+        env = problem.env
+
+        can = ParamSetup.setup_green_can()
+        can.pose = np.array([[0.763, -0.284, 1.067]]).T
+        can.rotation = np.array([[0,0,0]]).T
+
+        viewer = OpenRAVEViewer.create_viewer(env)
+        objLst = [i[1] for i in params.items() if not i[1].is_symbol()] + [can]
+        viewer.draw(objLst, 0, 0.7)
+        robot = params['baxter']
+        basket = params['basket']
+        table = params['table']
+        baxter_body = OpenRAVEBody(env, 'baxter', robot.geom)
+        basket_body = OpenRAVEBody(env, 'basket', basket.geom)
+        can_body = OpenRAVEBody(env, 'green_can', can.geom)
+        offset = [0,0.317,0]
+        basket_pos = basket.pose.flatten()
+
+        import ipdb; ipdb.set_trace()
+        left_arm_pose = baxter_body.get_ik_from_pose(basket_pos + offset, [0,np.pi/2,0], "left_arm")[0]
+
+        right_arm_pose = baxter_body.get_ik_from_pose(basket_pos - offset, [0,np.pi/2,0], "right_arm")[0]
+        baxter_body.set_dof({'lArmPose': left_arm_pose, "rArmPose": right_arm_pose})
+        import ipdb; ipdb.set_trace()
 
 
 if __name__ == "__main__":
