619c4ddf5861d0fecdad4097666dbf7955da18f5
Baxter Machine <m_j_mcdonald@comcast.net>
2017-07-08 10:38:52 -0700

    Action execution work

--- src/test/test_ros_interface/test_action_execution.py
+++ src/test/test_ros_interface/test_action_execution.py
@@ -22,13 +22,14 @@ class TestActionExecute(unittest.TestCase):
 		'''
 		# import ipdb; ipdb.set_trace()
 		pd = PlanDeserializer()
-		plan = pd.read_from_hdf5("basket_plan_with_putdown.hdf5")
+		plan = pd.read_from_hdf5("cloth_grasp_isolation.hdf5")
 		# baxter = plan.params['baxter']
 		# natural_state = np.array([0., 1.42, 0., 0.02, 0., 0.22, -0.]).reshape((7, 1))
 		# natural_traj = np.repeat(natural_state, 40, axis=1)
 		# baxter.lArmPose = natural_traj
 		# action = plan.actions[0]
 		# action_execution.execute_action(plan)
+		import ipdb; ipdb.set_trace()
 		print("Initializing node... ")
 		rospy.init_node("rsdk_joint_trajectory_client")
 		print("Getting robot state... ")
