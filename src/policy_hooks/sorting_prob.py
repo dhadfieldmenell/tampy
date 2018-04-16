af23d425d8057ec86545c69f8b62ebdd47a16560
Michael McDonald <m_J_mcdonald@berkeley.edu>
2018-04-15 21:07:37 -0700

    Switched to joint space

--- src/policy_hooks/sorting_prob.py
+++ src/policy_hooks/sorting_prob.py
@@ -17,11 +17,66 @@ targets = {
             'white_target': [0.7, -0.9, 0.65],
           }
 
+## Uncomment these two functions for more general tasks (use with sorting_domain and sorting_task_mapping)
+# def get_sorting_problem(cloth_locs, color_map):
+#     hl_plan_str = "(define (problem sorting_problem)\n"
+#     hl_plan_str += "(:domain sorting_domain)\n"
+
+#     hl_plan_str += "(:objects blue_target white_target yellow_target green_target basket_start_target"
+#     for cloth in color_map:
+#         hl_plan_str += " {0}".format(cloth)
+#     hl_plan_str += ")\n"
+
+#     hl_plan_str += parse_initial_state(cloth_locs)
+
+#     goal_str = "(:goal (and"
+#     for cloth in color_map:
+#         if color_map[cloth][0] == BLUE:
+#             goal_str += " (ClothAtLeftTarget {0} blue_target)".format(cloth)
+#         elif color_map[cloth][0] == WHITE:
+#             goal_str += " (ClothAtRightTarget {0} white_target)".format(cloth)
+#         elif color_map[cloth][0] == YELLOW:
+#             goal_str += " (ClothAtRightTarget {0} yellow_target)".format(cloth)
+#         elif color_map[cloth][0] == GREEN:
+#             goal_str += " (ClothAtLeftTarget {0} green_target)".format(cloth)
+#     goal_str += " (BasketAtTarget basket_start_target) "
+#     goal_str += "))\n"
+
+#     hl_plan_str += goal_str
+
+#     hl_plan_str += "\n)"
+#     return hl_plan_str, goal_str
+
+# def parse_initial_state(cloth_locs):
+#     hl_init_state = "(:init "
+#     for i in range(len(cloth_locs)):
+#         loc = cloth_locs[i]
+#         if loc[1] > 0:
+#             hl_init_state += " (ClothInLeftRegion Cloth{0})".format(i)
+#         else:
+#             hl_init_state += " (ClothInRightRegion Cloth{0})".format(i)
+
+#         for target in ['blue_target', 'green_target']:
+#             if np.all(np.abs(np.array(targets[target]) - loc) < 0.03):
+#                 hl_init_state += " (ClothAtLeftTarget Cloth{0} {1})".format(i, target)
+#             else:
+#                 hl_init_state += " (not (ClothAtLeftTarget Cloth{0} {1}))".format(i, target)
+        
+#         for target in ['white_target', 'yellow_target']:
+#             if np.all(np.abs(np.array(targets[target]) - loc) < 0.03):
+#                 hl_init_state += " (ClothAtRightTarget Cloth{0} {1})".format(i, target)
+#             else:
+#                 hl_init_state += " (not (ClothAtRightTarget Cloth{0} {1}))".format(i, target)
+#     hl_init_state += " (BasketAtTarget basket_start_target)"
+#     hl_init_state += ")\n"
+#     return hl_init_state
+
+
 def get_sorting_problem(cloth_locs, color_map):
     hl_plan_str = "(define (problem sorting_problem)\n"
     hl_plan_str += "(:domain sorting_domain)\n"
 
-    hl_plan_str += "(:objects blue_target white_target yellow_target green_target basket_start_target"
+    hl_plan_str += "(:objectst"
     for cloth in color_map:
         hl_plan_str += " {0}".format(cloth)
     hl_plan_str += ")\n"
@@ -31,14 +86,13 @@ def get_sorting_problem(cloth_locs, color_map):
     goal_str = "(:goal (and"
     for cloth in color_map:
         if color_map[cloth][0] == BLUE:
-            goal_str += " (ClothAtLeftTarget {0} blue_target)".format(cloth)
+            goal_str += " (ClothAtBlueTarget {0})".format(cloth)
         elif color_map[cloth][0] == WHITE:
-            goal_str += " (ClothAtRightTarget {0} white_target)".format(cloth)
+            goal_str += " (ClothAtWhiteTarget {0})".format(cloth)
         elif color_map[cloth][0] == YELLOW:
-            goal_str += " (ClothAtRightTarget {0} yellow_target)".format(cloth)
+            goal_str += " (ClothAtYellowTarget {0})".format(cloth)
         elif color_map[cloth][0] == GREEN:
-            goal_str += " (ClothAtLeftTarget {0} green_target)".format(cloth)
-    goal_str += " (BasketAtTarget basket_start_target) "
+            goal_str += " (ClothAtGreenTarget {0})".format(cloth)
     goal_str += "))\n"
 
     hl_plan_str += goal_str
@@ -55,29 +109,37 @@ def parse_initial_state(cloth_locs):
         else:
             hl_init_state += " (ClothInRightRegion Cloth{0})".format(i)
 
-        for target in ['blue_target', 'green_target']:
-            if np.all(np.abs(np.array(targets[target]) - loc) < 0.03):
-                hl_init_state += " (ClothAtLeftTarget Cloth{0} {1})".format(i, target)
-            else:
-                hl_init_state += " (not (ClothAtLeftTarget Cloth{0} {1}))".format(i, target)
+        if np.all(np.abs(np.array(targets['blue_target']) - loc) < 0.03):
+            hl_init_state += " (ClothAtBlueTarget Cloth{0})".format(i)
+        else:
+            hl_init_state += " (not (ClothAtBlueTarget Cloth{0}))".format(i)
         
-        for target in ['white_target', 'yellow_target']:
-            if np.all(np.abs(np.array(targets[target]) - loc) < 0.03):
-                hl_init_state += " (ClothAtRightTarget Cloth{0} {1})".format(i, target)
-            else:
-                hl_init_state += " (not (ClothAtRightTarget Cloth{0} {1}))".format(i, target)
-    hl_init_state += " (BasketAtTarget basket_start_target)"
+        if np.all(np.abs(np.array(targets['green_target']) - loc) < 0.03):
+            hl_init_state += " (ClothAtGreenTarget Cloth{0})".format(i)
+        else:
+            hl_init_state += " (not (ClothAtGreenTarget Cloth{0}))".format(i)
+        
+        if np.all(np.abs(np.array(targets['yellow_target']) - loc) < 0.03):
+            hl_init_state += " (ClothAtYellowTarget Cloth{0})".format(i)
+        else:
+            hl_init_state += " (not (ClothAtYellowTarget Cloth{0}))".format(i)
+        
+        if np.all(np.abs(np.array(targets['white_target']) - loc) < 0.03):
+            hl_init_state += " (ClothAtWhiteTarget Cloth{0})".format(i)
+        else:
+            hl_init_state += " (not (ClothAtWhiteTarget Cloth{0}))".format(i)
+        
     hl_init_state += ")\n"
     return hl_init_state
 
 def get_hl_plan(prob):
-    with open('../domains/laundry_domain/sorting_domain.pddl', 'r+') as f:
+    with open('../domains/laundry_domain/sorting_domain_2.pddl', 'r+') as f:
         domain = f.read()
     hl_solver = FFSolver(abs_domain=domain)
     return hl_solver._run_planner(domain, prob)
 
 def get_ll_plan_str(hl_plan, num_cloths):
-    tasks = get_tasks('policy_hooks/sorting_task_mapping')
+    tasks = get_tasks('policy_hooks/sorting_task_mapping_2')
     ll_plan_str = []
     actions_per_task = []
     last_pose = "ROBOT_INIT_POSE"
@@ -136,21 +198,33 @@ def get_target_state_vector(state_inds, goal_state, dX):
         preds[i] = preds[i].split()
         preds[i][-1] = preds[i][-1][:-1]
 
-        if preds[i][0] == "ClothAtLeftTarget" or preds[i][0] == "ClothAtRightTarget":
+        if preds[i][0] == "ClothAtBlueTarget":
             target = preds[i][2]
-            state[state_inds[(preds[i][1], 'pose')]] = targets[target]
+            state[state_inds[(preds[i][1], 'pose')]] = targets['blue_target']
+            weights[state_inds[(preds[i][1], 'pose')]] = 1.0
+        if preds[i][0] == "ClothAtGreenTarget":
+            target = preds[i][2]
+            state[state_inds[(preds[i][1], 'pose')]] = targets['green_target']
+            weights[state_inds[(preds[i][1], 'pose')]] = 1.0
+        if preds[i][0] == "ClothAtYellowTarget":
+            target = preds[i][2]
+            state[state_inds[(preds[i][1], 'pose')]] = targets['yellow_target']
+            weights[state_inds[(preds[i][1], 'pose')]] = 1.0
+        if preds[i][0] == "ClothAtWhiteTarget":
+            target = preds[i][2]
+            state[state_inds[(preds[i][1], 'pose')]] = targets['white_target']
             weights[state_inds[(preds[i][1], 'pose')]] = 1.0
     return state, weights
 
-def get_task_durations():
-    tasks = get_tasks('policy_hooks/sorting_task_mapping')
-    durations = []
-    for task in tasks:
-        for i in range(len(task)):
-            task[i].format('cloth0', 'blue_target', 'left_region', 0)
-        plan = plan_from_str(task[i])
-        durations.append(plan.horizon-1)
-    return durations
+# def get_task_durations():
+#     tasks = get_tasks('policy_hooks/sorting_task_mapping')
+#     durations = []
+#     for task in tasks:
+#         for i in range(len(task)):
+#             task[i].format('cloth0', 'blue_target', 'left_region', 0)
+#         plan = plan_from_str(task[i])
+#         durations.append(plan.horizon-1)
+#     return durations
 
 def fill_random_initial_configuration(plan):
     for param in plan.params:
