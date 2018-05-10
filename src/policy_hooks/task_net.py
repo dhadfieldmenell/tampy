67e608ef2126b5fce5a90005cf2368f20802e625
Michael McDonald <m_J_mcdonald@berkeley.edu>
2018-05-10 08:59:48 -0700

    Tak net

--- src/policy_hooks/task_net.py
+++ src/policy_hooks/task_net.py
@@ -43,9 +43,9 @@ def get_input_layer(dim_input, dim_output):
         action: mu, the ground truth actions we're trying to learn.
         precision: precision matrix used to commpute loss."""
     net_input = tf.placeholder("float", [None, dim_input], name='nn_input')
-    action = tf.placeholder('float', [None, dim_output], name='action')
+    task = tf.placeholder('float', [None, dim_output], name='task')
     precision = tf.placeholder('float', [None, dim_output, dim_output], name='precision')
-    return net_input, action, precision
+    return net_input, task, precision
 
 
 def get_mlp_layers(mlp_input, number_layers, dimension_hidden):
@@ -64,28 +64,17 @@ def get_mlp_layers(mlp_input, number_layers, dimension_hidden):
         if layer_step != number_layers-1:  # final layer has no RELU
             cur_top = tf.nn.relu(tf.matmul(cur_top, cur_weight) + cur_bias)
         else:
-            cur_top = tf.matmul(cur_top, cur_weight) + cur_bias
+            cur_top = sotfmax_prediction_layer(tf.matmul(cur_top, cur_weight) + cur_bias)
 
     return cur_top, weights, biases
 
 
-def get_loss_layer(mlp_out, action, precision, batch_size):
+def get_loss_layer(mlp_out, task):
     """The loss layer used for the MLP network is obtained through this class."""
-    return euclidean_loss_layer(a=action, b=mlp_out, precision=precision, batch_size=batch_size)
+    return softmax_loss_layer(labels=task, logits=mlp_out)
 
 
-def tf_network(dim_input=27, dim_output=7, batch_size=25, network_config=None):
-    """
-    Specifying a fully-connected network in TensorFlow.
-
-    Args:
-        dim_input: Dimensionality of input.
-        dim_output: Dimensionality of the output.
-        batch_size: Batch size.
-        network_config: dictionary of network structure parameters
-    Returns:
-        a TfMap object used to serialize, inputs, outputs, and loss.
-    """
+def tf_classification_network(dim_input=27, dim_output=7, batch_size=25, network_config=None):
     n_layers = 2 if 'n_layers' not in network_config else network_config['n_layers'] + 1
     dim_hidden = (n_layers - 1) * [40] if 'dim_hidden' not in network_config else network_config['dim_hidden']
     dim_hidden.append(dim_output)
@@ -93,6 +82,6 @@ def tf_network(dim_input=27, dim_output=7, batch_size=25, network_config=None):
     nn_input, action, precision = get_input_layer(dim_input, dim_output)
     mlp_applied, weights_FC, biases_FC = get_mlp_layers(nn_input, n_layers, dim_hidden)
     fc_vars = weights_FC + biases_FC
-    loss_out = get_loss_layer(mlp_out=mlp_applied, action=action, precision=precision, batch_size=batch_size)
+    loss_out = get_loss_layer(mlp_out=mlp_applied, task=task)
 
     return TfMap.init_from_lists([nn_input, action, precision], [mlp_applied], [loss_out]), fc_vars, []
