3b0fb447381b3302d06cb5b9e18d2e14d5e80320
Michael McDonald <m_J_mcdonald@berkeley.edu>
2018-08-08 23:30:33 -0700

    Updated rnn code

--- src/policy_hooks/rrn_net.py
+++ src/policy_hooks/rrn_net.py
@@ -49,10 +49,10 @@ def get_lstm_input_layer(dim_input, dim_output, lstm_steps):
         net_input: usually an observation.
         action: mu, the ground truth actions we're trying to learn.
         precision: precision matrix used to commpute loss."""
-    net_input = tf.placeholder("float", [None, dim_input], name='nn_input')
-    action_series = tf.placeholder('float', [None, dim_output, lstm_steps], name='action')
+    net_input = tf.placeholder("float", [None, lstm_steps, dim_input], name='nn_input')
+    action = tf.placeholder('float', [None, dim_output], name='action')
     precision = tf.placeholder('float', [None, dim_output, dim_output], name='precision')
-    return net_input, action_series, precision
+    return net_input, action, precision
 
 
 def get_mlp_layers(mlp_input, number_layers, dimension_hidden):
@@ -84,10 +84,10 @@ def get_stacked_lstm_cells(lstm_size, n_lstm_layers, keep_prob, name):
     return tf.contrib.run.MultiRNNCell([get_lstm_cell(lstm_size if type(lstm_size) is int else lstm_size[i], keep_prob, name+'_cell_{0}'.format(i)) for i in range(n_lstm_layers)], name)
 
 
-def get_lstm_layers(lstm_size, n_lstm_layers, lstm_steps, controls, keep_prob, batch_size):
-    stacked_lstm = get_stack_lstm_cells(lstm_size, n_lstm_layers, keep_prob, 'lstm_w')
-    initial_state = state = stacked_lstm.zero_state(batch_size, tf.float32)
-    lstm_out, final_state = tf.nn.dynamic_rnn(stacked_lstm, controls, initial_state=inisital_state)
+def get_lstm_layers(lstm_size, n_lstm_layers, lstm_steps, inputs, keep_prob, batch_size):
+    stacked_lstm = get_stacked_lstm_cells(lstm_size, n_lstm_layers, keep_prob, 'lstm_w')
+    initial_state = stacked_lstm.zero_state(batch_size, tf.float32)
+    lstm_out, final_state = tf.nn.dynamic_rnn(stacked_lstm, inputs, initial_state=inisital_state)
 
     return inital_state, final_state, lstm_out
 
@@ -101,13 +101,13 @@ def tf_lstm_network(dim_input, dim_output, batch_size, network_config=None):
     dim_hidden = (n_layers - 1) * [40] if 'dim_hidden' not in network_config else copy(network_config['dim_hidden'])
     dim_hidden.append(dim_output)
 
-    nn_inputs, action, action_series, precision = get_lstm_input_layer(dim_input, dim_output, lstm_steps)
+    nn_inputs, action, precision = get_lstm_input_layer(dim_input, dim_output, lstm_steps)
 
-    init_state, final_state, lstm_out = get_lstm_layers(lstm_size, n_lstm_layers, lstm_steps, action_series, batch_size)
+    init_state, final_state, lstm_out = get_lstm_layers(lstm_size, n_lstm_layers, lstm_steps, nn_inputs, batch_size)
     
-    top, weights, biases = get_mlp_layers(lstm_out[:,-1], n_layers, dim_hidden)
+    mlp_applied, weights, biases = get_mlp_layers(lstm_out[:,-1], n_layers, dim_hidden)
     fc_vars = weights + biases
 
-    loss_out = get_loss_layer(mlp_out=top, action=action_series[:,:,-1], precision=precision, batch_size=batch_size)
+    loss_out = get_loss_layer(mlp_out=mlp_applied, action=action, precision=precision, batch_size=batch_size)
  
-    return TfMap.init_from_lists([nn_inputs, action_series, precision], [top], [loss_out]), fc_vars, []
+    return TfMap.init_from_lists([nn_inputs, action, precision], [mlp_applied], [loss_out]), fc_vars, []
