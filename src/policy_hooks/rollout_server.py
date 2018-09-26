d7e611548e255f369ac31ec59be4f1c844030c05
Michael McDonald <m_J_mcdonald@berkeley.edu>
2018-09-26 14:31:57 -0700

    Updates to servers

--- src/policy_hooks/rollout_server.py
+++ src/policy_hooks/rollout_server.py
@@ -12,12 +12,13 @@ class dummy_policy:
     def __init__(self, task, policy_call):
         self.task = task
         self.policy_call = policy_call
+
     def act(self, x, obs, t, noise):
         return self.policy_call(x, obs, t, noise, self.task)
 
 
 class RolloutServer(object):
-    def __init__(self, mcts, algs, traj_opt_steps, n_samples, n_optimizers, max_sample_queue):
+    def __init__(self, mcts, algs, traj_opt_steps, n_samples, n_optimizers, max_sample_queue, max_opt_sample_queue):
         self.mcts = mcts
         self.alg_map = algs
         self.agent = mcts[0].agent
@@ -38,6 +39,7 @@ class RolloutServer(object):
         self.current_id = 0
         self.optimized = {}
         self.max_sample_queue = max_sample_queue
+        self.max_opt_sample_queue = max_opt_sample_queue
 
     def end(self, msg):
         self.stopped = True
@@ -100,15 +102,18 @@ class RolloutServer(object):
         self.current_id += 1
         while len(self.sample_queue) > self.max_sample_queue:
             del self.waiting_for_opt[self.sample_queue[0]]
-            self.sample_queue.pop(0)
+            del sample_queue[0]
 
 
-    def store_opt_sample(self, sample, plan_id):
+    def store_opt_smaple(self, sample, plan_id):
         if plan_id in self.waiting_for_opt:
             samples = self.waiting_for_opt[plan_id]
         else:
             samples = []
+
         self.opt_samples.append((sample, samples))
+        while len(self.opt_samples) > self.max_opt_sample_queue:
+            del self.opt_samples[0]
 
 
     def sample_mp(self, msg):
@@ -116,25 +121,32 @@ class RolloutServer(object):
         traj = np.array([msg.traj[i].data for i in range(len(msg.traj))])
         success = msg.success
         failed = eval(msg.failed)
+        task = msg.task
+        condition = msg.condition
+        obj = self.agent.plans.values()[0].params[msg.obj]
+        targ = self.agent.plans.values()[0].params[msg.targ]
         if success:
-            opt_sample = self.agent.sample_optimal_trajectory()
-            self.store_opt_sample(opt_sample, plan_id)
+            opt_sample = self.agent.sample_optimal_trajectory(traj[0], task, condition, traj, traj_mean=[], fixed_targets=[obj, targ])
+            self.store_opt_traj(opt_sample, plan_id)
 
 
     def step(self):
-        rollout_policies = {task: dummy_policy(task, self.policy_call).act for task in self.agent.task_list}
+        rollout_policies = {task: dummy_policy(task, self.policy_call) for task in self.agent.task_list}
 
-        self.mcts[cond].run(self.agent.x0[self.cond], self.n_samples, False, new_policies=rollout_policies, debug=True)
+        for mcts in self.mcts:
+            mcts.run(self.agent.x0[mcts.condition], self.n_samples, False, new_policies=rollout_policies, debug=True)
 
         sample_lists = {task: self.agent.get_samples(task) for task in self.task_list}
-        self.agent.clear_samples(keep_prob=0.2, keep_opt_prob=0.2)
+        self.agent.clear_samples(keep_prob=0.1, keep_opt_prob=0.2)
 
-        for step in range(self.traj_opt_steps):
+        for step in range(self.traj_opt_steps-1):
             for task in self.agent.task_list:
                 try:
                     sample_lists[task] = self.alg_map[task].iteration(sample_lists[task], self.agent.optimal_samples[task], reset=not step)
-                    if len(sample_lists[task]) and step < traj_opt_steps - 1:
+                    if len(sample_lists[task]):
                         sample_lists[task] = self.agent.resample(sample_lists[task], rollout_policies[task], self.n_samples)
+                    else:
+                        continue
                     self.agent._samples[task] = sample_lists[task]
                 except:
                     traceback.print_exception(*sys.exc_info())
