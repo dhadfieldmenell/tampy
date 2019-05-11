6b8c63261729ff3d1a6bde20eb187cb96984220e
michaelmcdonald <m_j_mcdonald@berkeley.edu>
2019-05-11 16:49:44 -0700

    Updated launch scripts

--- src/policy_hooks/vae/launch_namo.sh
+++ src/policy_hooks/vae/launch_namo.sh
@@ -1,12 +1,13 @@
-(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -vae -id 0) &
-(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 0) &
-(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 1) &
-(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 2) &
-(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 3) &
-(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 4) &
-(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 5) &
-(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 6) &
-(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 7) &
-(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 8) &
-(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 9) &
-(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 10) &
+RL=20
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -rl $RL -ncp -vae -id 0) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -rl $RL -ncp -sim -id 0) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -rl $RL -ncp -sim -id 1) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -rl $RL -ncp -sim -id 2) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -rl $RL -ncp -sim -id 3) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -rl $RL -ncp -sim -id 4) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -rl $RL -ncp -sim -id 5) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -rl $RL -ncp -sim -id 6) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -rl $RL -ncp -sim -id 7) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -rl $RL -ncp -sim -id 8) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -rl $RL -ncp -sim -id 9) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -rl $RL -ncp -sim -id 10) &
