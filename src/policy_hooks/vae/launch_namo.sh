9a3be29e2feeb983f5c3ee39b72d775580fa0ae1
Michael McDonald <m_J_mcdonald@berkeley.edu>
2019-05-10 17:42:42 -0700

    Updated launch script for namo

--- src/policy_hooks/vae/launch_namo.sh
+++ src/policy_hooks/vae/launch_namo.sh
@@ -1,12 +1,12 @@
-python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -vae -id 0 &
-python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 0 &
-python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 1 &
-python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 2 &
-python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 3 &
-python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 4 &
-python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 5 &
-python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 6 &
-python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 7 &
-python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 8 &
-python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 9 &
-python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 10 &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -vae -id 0) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 0) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 1) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 2) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 3) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 4) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 5) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 6) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 7) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 8) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 9) &
+(sleep 1 ; python policy_hooks/vae/run_training.py --config namo.namo_hyperparams -n 1 -o 5 -ncp -sim -id 10) &
