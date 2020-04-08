d316ad2274a7a24a343d7ebf83e9cc2514b3554a
michaelmcdonald <m_j_mcdonald@berkeley.edu>
2020-04-08 05:03:13 -0700

    Fixed queue error

--- src/policy_hooks/namo/namo_agent.py
+++ src/policy_hooks/namo/namo_agent.py
@@ -341,7 +341,7 @@ class NAMOSortingAgent(TAMPAgent):
                 dists[i] = dists[i] if is_hits[i] else LIDAR_DIST
         else:
             P.stepSimulation()
-            _, _, hit_frac, hit_pos, hit_normal = P.rayBatchTest(rays[:,:3], rays[:,:3]+rays[:,3:])
+            _, _, hit_frac, hit_pos, hit_normal = P.rayTestBatch(rays[:,:3], rays[:,:3]+rays[:,3:])
             dists = LIDAR_DIST * hit_frac
 
         # dists[np.abs(dists) > LIDAR_DIST] = LIDAR_DIST
