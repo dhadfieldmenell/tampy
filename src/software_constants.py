8b6373e22d894e0f722400cdda637f177d160d7a
michaelmcdonald <m_j_mcdonald@berkeley.edu>
2020-04-10 02:13:14 -0700

    Fixed bug in iter

--- src/software_constants.py
+++ src/software_constants.py
@@ -1,3 +1,15 @@
 USE_OPENRAVE = True
 USE_ROS = True
 
+try:
+    import rospy
+except Exception as e:
+    print(e)
+    USE_ROS = False
+
+try:
+    import openravepy
+except Exception as e:
+    print(e)
+    USE_OPENRAVE = False
+
