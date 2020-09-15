19409df8b487e5655822779488377f46e64cd241
michaelmcdonald <m_j_mcdonald@berkeley.edu>
2020-09-15 16:36:51 -0700

    Improved setup

--- admin_setup.sh
+++ admin_setup.sh
@@ -1,5 +1,6 @@
 # This contains necessary commands to install system-wide dependencies, but needs sudo priveleges to run
 
+sudo apt-get update
 sudo apt-get install python3-pip virtualenv
 sudo apt-get install cmake libopenmpi-dev python3-dev zlib1g-dev
 sudo apt-get install libglew2.0 libgl1-mesa-glx libosmesa6
