7d41cb0f86cc8e0020a170885a0a94b8bed2913c
michaelmcdonald <m_j_mcdonald@berkeley.edu>
2020-10-20 14:56:49 -0700

    Add example baseline run

---
+++ src/example_baseline.sh
@@ -0,0 +1,19 @@
+for N in 1 2 3 4 5
+do
+    for T in 2000 3000
+    do
+        python3 -W ignore policy_hooks/run_training.py \
+                -c policy_hooks.namo.hyperparams_v93 \
+                -no 1 -nt 1 \
+                -baseline hiro \
+                --traj_limitation ${T} \
+                --index ${N} \
+                --num_timesteps 5000000 \
+                --episode_timesteps 200\
+                -descr hiro_baseline_1obj_${T}_trajectories & 
+        sleep 3h
+        pkill -f run_train -9
+        sleep 5s
+    done
+done
+
