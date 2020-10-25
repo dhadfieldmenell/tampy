9c377b96ee97dcde615c114bc05bd31ae9304fa7
michaelmcdonald <m_j_mcdonald@berkeley.edu>
2020-10-25 04:27:56 -0700

    Updtaes to namo

--- src/core/util_classes/namo_predicates.py
+++ src/core/util_classes/namo_predicates.py
@@ -1186,7 +1186,7 @@ class Obstructs(CollisionPredicate):
 class WideObstructs(Obstructs):
     def __init__(self, name, params, expected_param_types, env=None, debug=False):
         super(WideObstructs, self).__init__(name, params, expected_param_types, env, debug=debug)
-        self.dsafe = 1.2
+        self.dsafe = 0.5
 
 def sample_pose(plan, pose, robot, rs_scale):
     targets  = plan.get_param('InContact', 2, {0: robot, 1:pose})
@@ -1455,7 +1455,7 @@ class ObstructsHolding(CollisionPredicate):
 class WideObstructsHolding(ObstructsHolding):
     def __init__(self, name, params, expected_param_types, env=None, debug=False):
         super(WideObstructsHolding, self).__init__(name, params, expected_param_types, env, debug)
-        self.dsafe = 1.5
+        self.dsafe = 0.7
 
 
 class InGripper(ExprPredicate):
