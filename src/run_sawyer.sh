d45711e9275b03e9346223d58096b54e6e732f99
Ubuntu <azureuser@Baselines.fszexv4trnguzfbq0vmj2xen2c.bx.internal.cloudapp.net>
2021-04-15 01:06:12 +0000

    Update on run script

--- src/run_sawyer.sh
+++ src/run_sawyer.sh
@@ -1,25 +1,22 @@
 for N in 1 2 3 4 5
 do
 
-
 	python3.6 -W ignore policy_hooks/run_training.py -c policy_hooks.robosuite.jnt_pick_hyp \
-                                                       -no 1 -nt 1 -llus 3000  -hlus 3000 \
+                                                       -no 1 -nt 1 -llus 2000  -hlus 6000 \
                                                        -ff 1. -mask -hln 2 -lln 2 -hldim 64 -lldim 64 \
                                                        -eta 5 \
-                                                       -lr_policy adaptive \
-                                                       -hist_len 2 -prim_first_wt 10 -lr 0.0002 \
+                                                       -hist_len 1 -prim_first_wt 10 -lr 0.0002 \
                                                        -hllr 0.0002 -lldec 0.0000 -hldec 0.0001 \
                                                        -add_noop 0 --permute_hl 0 \
                                                        -post -pre \
                                                        -expl_wt 5 -expl_eta 4 \
-                                                       -neg -neg_pre -neg_post \
                                                        -motion 20 \
                                                        -task 2 \
-                                                       -rollout 8 \
+                                                       -rollout 10 \
                                                        --load_render \
                                                        -warm 200 \
-                                                       -backup -verbose \
-                                                       -descr wed_backup_dagger& 
+						       -verbose \
+                                                       -descr wed_nobackup_dagger& 
         sleep 4h
         pkill -f run_train -9
         pkill -f ros -9
@@ -27,22 +24,21 @@ do
 
 
 	python3.6 -W ignore policy_hooks/run_training.py -c policy_hooks.robosuite.jnt_pick_hyp \
-                                                       -no 1 -nt 1 -llus 3000  -hlus 3000 \
+                                                       -no 1 -nt 1 -llus 2000  -hlus 6000 \
                                                        -ff 1. -mask -hln 2 -lln 2 -hldim 64 -lldim 64 \
                                                        -eta 5 \
-                                                       -lr_policy adaptive \
-                                                       -hist_len 2 -prim_first_wt 10 -lr 0.0002 \
+                                                       -hist_len 1 -prim_first_wt 10 -lr 0.0002 \
                                                        -hllr 0.0002 -lldec 0.0000 -hldec 0.0001 \
                                                        -add_noop 0 --permute_hl 0 \
                                                        -post -pre \
                                                        -expl_wt 5 -expl_eta 4 \
                                                        -motion 20 \
-                                                       -neg -neg_pre -neg_post \
                                                        -task 2 \
-                                                       -rollout 8 \
+                                                       -rollout 10 \
                                                        --load_render \
                                                        -warm 200 \
-                                                       -descr wed_nobackup_dagger& 
+						       -backup -verbose \
+                                                       -descr wed_backup_dagger& 
         sleep 4h
         pkill -f run_train -9
         pkill -f ros -9
