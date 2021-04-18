2344b0d51e8e37cbefe4eec608025224bff15d08
Ubuntu <azureuser@Baselines.fszexv4trnguzfbq0vmj2xen2c.bx.internal.cloudapp.net>
2021-04-18 21:22:17 +0000

    Update on sawyer script

--- src/run_sawyer.sh
+++ src/run_sawyer.sh
@@ -1,6 +1,7 @@
 for N in 1 2 3 4 5
 do
 
+
 	python3.6 -W ignore policy_hooks/run_training.py -c policy_hooks.robosuite.jnt_pick_hyp \
                                                        -no 1 -nt 1 -llus 2000  -hlus 6000 \
                                                        -ff 1. -mask -hln 2 -lln 2 -hldim 64 -lldim 64 \
@@ -16,7 +17,9 @@ do
                                                        --load_render \
                                                        -warm 200 \
 						       -verbose \
-                                                       -descr wed_nobackup_dagger& 
+						       -neg_pre -neg_post \
+						       -neg_ratio 0.05 -opt_ratio 0.475 -dagger_ratio 0.475 -roll_ratio 0.000 \
+                                                       -descr random_init_fiveperc_negative_dagger& 
         sleep 4h
         pkill -f run_train -9
         pkill -f ros -9
@@ -37,12 +40,15 @@ do
                                                        -rollout 10 \
                                                        --load_render \
                                                        -warm 200 \
-						       -backup -verbose \
-                                                       -descr wed_backup_dagger& 
-        sleep 4h
+						       -verbose \
+						       -neg_pre -neg_post \
+						       -neg_ratio 0.0 -opt_ratio 0.5 -dagger_ratio 0.5 -roll_ratio 0.000 \
+                                                       -descr random_init_dagger& 
+        sleep 5h
         pkill -f run_train -9
         pkill -f ros -9
         sleep 5s
+
 
 
 done
